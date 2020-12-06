d17958ca57633decd46efc057d257df75d388262
omardwwm <boudraa.omar@gmail.com>
2020-12-06 20:23:42 +0100

    add season

---
+++ public/js/javascript.js
@@ -0,0 +1,7 @@
+let colorMounth = document.getElementById('listMois')
+let idMois = document.getElementById('moi.id').value;
+console.log(idMois);
+console.log(colorMounth);
+if (5<10){
+    colorMounth.style.backgroundColor = '#000000';
+}
