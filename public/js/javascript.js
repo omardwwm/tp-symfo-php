ebae85d9564f32d624fa881cc28d150d72fe6f81
omardwwm <boudraa.omar@gmail.com>
2020-12-16 04:37:27 +0100

    Add ds modif

--- public/js/javascript.js
+++
@@ -1,7 +0,0 @@
-let colorMounth = document.getElementById('listMois')
-let idMois = document.getElementById('moi.id').value;
-console.log(idMois);
-console.log(colorMounth);
-if (5<10){
-    colorMounth.style.backgroundColor = '#000000';
-}
